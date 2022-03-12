d04a4def07275960bdfd8e387b092c9b3b25ab6b
eozturk@microstrategy.com <haktan_emir@yahoo.com>
2022-03-12 14:30:18 +0100

    initial preparation

---
+++ src/Components/App.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+function App() {
+  const classStyle = {
+    display: "flex",
+    justifyContent: "center",
+    width: "100vw",
+    height: "100vh",
+    fontSize: 50,
+  };
+
+  return (
+    <div style={classStyle}>
+      <span>Project Template</span>
+    </div>
+  );
+}
+
+export default App;
