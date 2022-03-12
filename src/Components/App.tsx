26cf187fd290871cc831b82cb9ea2d160f292ccd
eozturk@microstrategy.com <haktan_emir@yahoo.com>
2022-03-12 14:46:19 +0100

    packages and styling

--- src/Components/App.tsx
+++ src/Components/App.tsx
@@ -1,19 +1,11 @@
 import React from "react";
 
-function App() {
-  const classStyle = {
-    display: "flex",
-    justifyContent: "center",
-    width: "100vw",
-    height: "100vh",
-    fontSize: 50,
-  };
+import "./app.scss";
 
-  return (
-    <div style={classStyle}>
-      <span>Project Template</span>
-    </div>
-  );
-}
+const App = () => (
+  <div className="template">
+    <span>Project Template</span>
+  </div>
+);
 
 export default App;
